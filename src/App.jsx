da0be5433681f393b0e9c3e47f56dcc6ead46c00
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 21:00:40 +0900

    Enhance App component to manage selected study item and pass selection handler to StudyList

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import reactData from "./data/data.json";
 import StudyInfo from "./components/StudyInfo";
 import { Box } from "@mui/material";
 import StudyList from "./components/StudyList";
+import { useState } from "react";
 
 console.log(reactData);
 
 function App() {
+  const [selectedId, setSelectedId] = useState(null);
+  const handleSelect = id => {
+    setSelectedId(id);
+  };
+
   return (
     <div className="App">
       <h1>React Basic Review Mission 2</h1>
@@ -15,7 +21,7 @@ function App() {
 
       <h3>학습 목록</h3>
 
-      <StudyList items={reactData} />
+      <StudyList items={reactData} selectedId={selectedId} onSelect={handleSelect} />
     </div>
   );
 }
