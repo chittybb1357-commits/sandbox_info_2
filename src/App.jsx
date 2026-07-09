7d999ce0703882d3cbd0875db0812ec09f37da95
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 17:06:41 +0900

    Implement App component to display React review mission and item count

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import reactData from "./data/data.json";
+
+console.log(reactData);
+
+const rd = reactData;
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <h1>React Basic Review Mission 2</h1>
+
+      <p>전체 학습 항목 수: {rd.length}개</p>
+    </div>
+  );
 }
 
 export default App;
