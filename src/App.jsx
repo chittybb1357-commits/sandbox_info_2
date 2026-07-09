a32482e46d87e5ffa85453aa4985a2c752529427
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 22:21:41 +0900

    Enhance App component to support keyword filtering in StudyList

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import reactData from "./data/data.json";
 import StudyList from "./components/StudyList";
 import { useState } from "react";
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, TextField } from "@mui/material";
 
 function App() {
   const [selectedId, setSelectedId] = useState(null);
   const [category, setCategory] = useState("all");
+  const [keyword, setKeyword] = useState("");
   const handleSelect = id => {
     if (selectedId === id) {
       setSelectedId(null);
@@ -15,7 +16,12 @@ function App() {
     }
   };
   const filteredItems = () => {
-    return category === "all" ? reactData : reactData.filter(item => item.category === category);
+    return reactData.filter(item => {
+      const matchesCategory = category === "all" || item.category === category;
+      const matchesKeyword = item.title.toLowerCase().includes(keyword.toLowerCase());
+
+      return matchesCategory && matchesKeyword;
+    });
   };
   const categories = [
     { value: "all", label: "전체" },
@@ -26,16 +32,10 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Stack 
-        direction="row" 
-        spacing={2} 
-        sx={{ 
-          mb: 3, 
-          justifyContent: "flex-start", 
-          alignItems: "center", 
-          mt: 3 
-        }}
-      > */}
+      <h1>React Basic Review Mission 2</h1>
+
+      <p>전체 학습 항목 수: {filteredItems().length}개</p>
+
       <Stack direction="row" spacing={1} sx={{ mb: 3, justifyContent: "flex-start", mt: 3 }}>
         {categories.map(cat => (
           <Button
@@ -47,13 +47,17 @@ function App() {
             {cat.label}
           </Button>
         ))}
-
-        {/* <p style={{ margin: 0, marginLeft: "auto", fontSize: "0.95rem", color: "#777777" }}> */}
       </Stack>
 
-      <h1>React Basic Review Mission 2</h1>
-
-      <p>전체 학습 항목 수: {filteredItems().length}개</p>
+      <TextField
+        fullWidth
+        variant="outlined"
+        size="small"
+        placeholder="검색할 제목을 입력하세요..."
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        sx={{ mb: 3, backgroundColor: "#ffffff", borderRadius: "4px" }}
+      />
 
       <h3>학습 목록</h3>
 
