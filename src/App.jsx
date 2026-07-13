b69711bc05c6f37165826a18a1b8aefac32c1e4d
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-13 16:35:28 +0900

    Import StudyInfo component and update App layout to display study details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import reactData from "./data/data.json";
-// import StudyInfo from "./components/StudyInfo";
+import StudyInfo from "./components/StudyInfo";
 // import StudyList from "./components/StudyList";
 // import { Box, Button, Stack, TextField } from "@mui/material";
 // import { useState } from "react";
@@ -8,10 +8,15 @@ import reactData from "./data/data.json";
 function App() {
   console.log(reactData);
 
+  const m2Content = reactData[0];
+
   return (
     <>
       <h1>React Mission 7</h1>
+
       <p>전체 학습 항목수 : {reactData.length}개</p>
+      <hr />
+      <StudyInfo title={m2Content.title} desc={m2Content.desc} category={m2Content.category} />
     </>
   );
 }
