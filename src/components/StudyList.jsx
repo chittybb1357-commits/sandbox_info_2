796c8832baf1b9de29c5ba3492e642b3f65a6045
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-13 16:47:31 +0900

    Implement StudyList component to display items with details

--- src/components/StudyList.jsx
+++ src/components/StudyList.jsx
@@ -1,3 +1,20 @@
-export default function StudyList() {
-  return <h2>학습 목록</h2>;
+export default function StudyList({ items }) {
+  console.log(items);
+
+  return (
+    <>
+      <h2>학습 목록</h2>
+      <section>
+        {items.map(item => (
+          <article key={item.id}>
+            <h3>{item.title}</h3>
+
+            <p>{item.desc}</p>
+
+            <p>분류: {item.category}</p>
+          </article>
+        ))}
+      </section>
+    </>
+  );
 }
